c6b33d271cf398902d6a1e62513435d745639136
DINESH B <115802283+dinesh-fullstackwebdeveloper@users.noreply.github.com>
2024-11-18 11:04:10 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+// ------------------- login Page ------------------------------------
+
+var checkbox = document.getElementById("show");
+var password = document.getElementById("pass");
+
+checkbox.addEventListener('click',()=>{
+    var val = password.getAttribute("type") === "password" ? "text" : "password";
+    password.setAttribute("type",val);
+});
